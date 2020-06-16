d93a9e7b7644c25b161cd6752fb25e234cf02930
minds <minds@localhost.localdomain>
2020-06-16 15:09:27 +0900

    Server-side

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-#! /usr/bin/zsh
+#! /usr/bin/bash
+
+source env/bin/activate
 
 echo "Generating preconfigured ksx1001 dataset!"
 echo "224x224, random color, random background, 2350 classes, random fonts"
@@ -7,15 +9,15 @@ COUNT20=20000
 RTRAIN=17
 RVALID=2
 RTESTS=1
-DIR="out/test/"
+DIR="out/ksx/"
 FONTDIR="resources/fonts"
-THREADS=32
+THREADS=128
 
 echo "Generating train dataset"
-./run.py --height 224 --width 224 --rand_color --rand_back -t $THREADS --output_dir $DIR/train --font_dir $FONTDIR --count $(expr $COUNT20 "*" $RTRAIN)
+python3 run.py --height 224 --width 224 --rand_color --rand_back -t $THREADS --output_dir $DIR/train --font_dir $FONTDIR --count $(expr $COUNT20 "*" $RTRAIN)
 
 echo "Generating validation dataset"
-./run.py --height 224 --width 224 --rand_color --rand_back -t $THREADS --output_dir $DIR/valid --font_dir $FONTDIR --count $(expr $COUNT20 "*" $RVALID)
+python3 run.py --height 224 --width 224 --rand_color --rand_back -t $THREADS --output_dir $DIR/valid --font_dir $FONTDIR --count $(expr $COUNT20 "*" $RVALID)
 
 echo "Generating test dataset"
-./run.py --height 224 --width 224 --rand_color --rand_back -t $THREADS --output_dir $DIR/tests --font_dir $FONTDIR --count $(expr $COUNT20 "*" $RTESTS)
+python3 run.py --height 224 --width 224 --rand_color --rand_back -t $THREADS --output_dir $DIR/tests --font_dir $FONTDIR --count $(expr $COUNT20 "*" $RTESTS)
